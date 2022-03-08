df5a0cef7a11fead23b73763753d4e0e446c10c4
mazurenka <an.drej@mail.ru>
2022-03-08 16:59:28 +0100

    1

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -4,14 +4,17 @@ import styleContainer from '../common/styles/Container.module.css'
 
 function Contacts() {
     return (
+
         <div className={style.contactsBlock}>
+
             <div className={styleContainer.container}>
-                <div className={style.text}>
-                    <span>Hi There</span>
-                    <h1>I am Andrei Mazurenka</h1>
-                    <p>Front-end Developer</p>
-                </div>
-                <div className={style.photo}></div>
+                <h2 className={style.title}>Contacts</h2>
+                <form>
+                    <input type={'text'}/>
+                    <input type={'text'}/>
+
+                    <textarea></textarea>
+                </form>
             </div>
 
         </div>
