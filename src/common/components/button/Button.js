83d2248cf24db6d7a6a6ad41872a0d9c7a5d0602
mazurenka <an.drej@mail.ru>
2022-03-14 17:39:32 +0100

    3

---
+++ src/common/components/button/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from './Button.module.scss'
+
+function Button (props) {
+    return (
+        <a href={''} className={style.btn} >{props.text}</a>
+    )
+}
